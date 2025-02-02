7a95a0ba6e7b2eefcc7441f03408d25dcf8d9305
Todor Mitev <td_mitev@abv.bg>
2025-02-02 15:39:04 +0200

    Initialise ReVitaClinic app

---
+++ src/main/java/com/example/revitaclinic/ReVitaClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.revitaclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReVitaClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReVitaClinicApplication.class, args);
+	}
+
+}
